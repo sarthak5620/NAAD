9136caecfb89681cf760df6a72edeb3058eadca8
Sarthak Khandelwal <66621092+sarthak5620@users.noreply.github.com>
2021-03-26 19:01:48 +0530

    commit

---
+++ src/task0/AddIntegers.java
@@ -0,0 +1,15 @@
+package task0;
+import java.util.Scanner;
+
+public class AddIntegers {
+    public static void main(String[] args) {
+        int n1,n2,n3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number:");
+        n1=sc.nextInt();
+        System.out.println("enter second number:");
+        n2=sc.nextInt();
+        n3 = n1 + n2 ;
+        System.out.println("the sum of entered numbers is:" + n3);
+    }
+}
